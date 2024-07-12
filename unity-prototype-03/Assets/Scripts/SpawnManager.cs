e997df2c2f924fa4ac4079c086df6fe382f23f34
habner-gabryel <habner.gabryel.cc@gmail.com>
2024-07-12 18:08:23 -0300

    3.1.9 commit

--- unity-prototype-03/Assets/Scripts/SpawnManager.cs
+++ unity-prototype-03/Assets/Scripts/SpawnManager.cs
@@ -7,15 +7,22 @@ public class SpawnManager : MonoBehaviour
     public GameObject obstaclePrefab;
     private Vector3 spawnPos = new Vector3(25,0,0);
 
+    private float startDelay = 4;
+    private float repeatRate = 2;
+
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        InvokeRepeating("spawnObstacle", startDelay, repeatRate);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void spawnObstacle(){
+        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
     }
 }
