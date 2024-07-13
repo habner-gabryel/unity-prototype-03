82d8aeecff42136a6531ceb970a9dd94e39593e7
habner-gabryel <habner.gabryel.cc@gmail.com>
2024-07-13 12:14:13 -0300

    3.3.3 commit

--- unity-prototype-03/Assets/Scripts/PlayerController.cs
+++ unity-prototype-03/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
+    private Animator playerAnim;
     public float jumpForce  = 10F;
     public float gravityModifier;
     public bool isOnGround = true;
@@ -14,6 +15,7 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
         gameOver = false;
     }
@@ -23,7 +25,10 @@ public class PlayerController : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.Space) && isOnGround){
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            playerAnim.SetTrigger("Jump_trig");
             isOnGround = false;
+        } else if(Input.GetKeyUp(KeyCode.Space) && !isOnGround){
+            
         }
     }
 
